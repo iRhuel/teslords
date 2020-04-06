057ec8b6b93f5bfc2f2f37de89abe07fea18a4f7
Phil Truong <pltruong2507@gmail.com>
2020-04-06 11:33:27 -0700

    delete dist folder

--- dist/utils/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Tesla_1 = require("./Tesla");
-exports.Tesla = Tesla_1.default;
-var usePassport_1 = require("./usePassport");
-exports.usePassport = usePassport_1.default;
-//# sourceMappingURL=index.js.map
--- dist/utils/kWh.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=kWh.js.map
